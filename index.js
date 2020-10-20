44987ca1b74ee5e48c5123d7643a8f191b4e8034
malenarey <malena.rey@contabilium.com>
2020-10-19 23:19:42 -0300

    fc

---
+++ index.js
@@ -0,0 +1,49 @@
+// numeros
+// strings
+// booleanos
+// arrays 
+
+const nombres = ["Caro", "Rebe", "Angie", "Maca"]
+// propiedades
+
+// .length -- > un numero con la cantidad de elementos dentro del array
+
+
+// Metodos de array
+// Push: agregar un elemento al final del array
+// nombreDelArray.push("lo que quiera agregar al array")
+
+
+// Sintaxis de for: largo y abreviado 
+
+// //for 
+// for (let i = 0; i < nombres.length; i++) {
+//   console.log(nombres[i])
+// }
+
+
+
+// // for of 
+// for (let nombre of nombres) {
+//   console.log(nombre)
+// }
+
+
+// const obtenerFrutaAlAzar = () => {
+//  return 'apple'
+// }
+
+// let listaDeFrutas = []
+
+// listaDeFrutas.push(obtenerFrutaAlAzar())
+// listaDeFrutas.push(obtenerFrutaAlAzar())
+
+
+// console.log(listaDeFrutas)
+
+
+for (let i = 0; i < 3; i++) {
+
+  console.log("hola")
+
+}
